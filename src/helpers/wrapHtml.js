5ab5825467c38a37852abe11d9c0f78242eedec2
Irek Nazmiev <ireknazmievirek@gmail.com>
2021-03-19 17:43:21 +0300

    Restructure the project * Move the code to src folder

--- src/helpers/wrapHtml.js
+++ src/helpers/wrapHtml.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const prettier = require('prettier');
-const prettierConfig = require('../.prettierrc');
+const prettierConfig = require('../../.prettierrc');
 
-const template = fs.readFileSync('helpers/template.html', 'utf8');
+const template = fs.readFileSync('src/template.html', 'utf8');
 
 module.exports = (body, { wrapper = template, config = prettierConfig } = {}) =>
     prettier.format(wrapper.replace('<div id="root" />', body), {
